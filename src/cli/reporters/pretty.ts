96765e36bcd154702184ab88a4c5891911d54985
Bart Veneman <bartveneman@gmail.com>
2025-10-29 19:16:33 +0100

    chore: refactor pretty printer

--- src/cli/reporters/pretty.ts
+++ src/cli/reporters/pretty.ts
@@ -8,13 +8,26 @@ function indent(line?: string): string {
 	return (line || '').replace(/^\t+/, (tabs) => ' '.repeat(tabs.length * 4))
 }
 
-export function print({ report, context }: Report, params: CliArguments) {
+function percentage(ratio: number): string {
+	return `${(ratio * 100).toFixed(2)}%`
+}
+
+type TextStyle = 'bold' | 'red' | 'dim' | 'green'
+type StyleTextFn = (style: TextStyle | TextStyle[], input: string) => string
+
+export function print_lines(
+	{ report, context }: Report,
+	params: CliArguments,
+	{ styleText, print_width }: { styleText: StyleTextFn; print_width?: number },
+) {
+	let output: (string | undefined)[] = []
+
 	if (report.min_line_coverage.ok) {
-		console.log(`${styleText(['bold', 'green'], 'Success')}: total line coverage is ${(report.min_line_coverage.actual * 100).toFixed(2)}%`)
+		output.push(`${styleText(['bold', 'green'], 'Success')}: total line coverage is ${percentage(report.min_line_coverage.actual)}%`)
 	} else {
-		console.error(
-			`${styleText(['bold', 'red'], 'Failed')}: line coverage is ${(report.min_line_coverage.actual * 100).toFixed(
-				2,
+		output.push(
+			`${styleText(['bold', 'red'], 'Failed')}: line coverage is ${percentage(
+				report.min_line_coverage.actual,
 			)}% which is lower than the threshold of ${report.min_line_coverage.expected}`,
 		)
 	}
@@ -22,16 +35,16 @@ export function print({ report, context }: Report, params: CliArguments) {
 	if (report.min_file_line_coverage.expected !== undefined) {
 		let { expected, actual, ok } = report.min_file_line_coverage
 		if (ok) {
-			console.log(`${styleText(['bold', 'green'], 'Success')}: all files pass minimum line coverage of ${expected * 100}%`)
+			output.push(`${styleText(['bold', 'green'], 'Success')}: all files pass minimum line coverage of ${percentage(expected)}%`)
 		} else {
 			let num_files_failed = context.coverage.coverage_per_stylesheet.filter((sheet) => sheet.line_coverage_ratio < expected!).length
-			console.error(
-				`${styleText(['bold', 'red'], 'Failed')}: ${num_files_failed} files do not meet the minimum line coverage of ${
-					expected * 100
-				}% (minimum coverage was ${(actual * 100).toFixed(2)}%)`,
+			output.push(
+				`${styleText(['bold', 'red'], 'Failed')}: ${num_files_failed} ${
+					num_files_failed === 1 ? 'file does' : 'files do'
+				} not meet the minimum line coverage of ${percentage(expected)} (minimum coverage was ${percentage(actual)})`,
 			)
 			if (params['show-uncovered'] === 'none') {
-				console.log(`  Hint: set --show-uncovered=violations to see which files didn't pass`)
+				output.push(`  Hint: set --show-uncovered=violations to see which files didn't pass`)
 			}
 		}
 	}
@@ -40,7 +53,7 @@ export function print({ report, context }: Report, params: CliArguments) {
 	if (params['show-uncovered'] !== 'none') {
 		const NUM_LEADING_LINES = 3
 		const NUM_TRAILING_LINES = NUM_LEADING_LINES
-		let terminal_width = process.stdout.columns || 80
+		print_width = print_width ?? 80
 		let line_number = (num: number, covered: boolean = true) => `${num.toString().padStart(5, ' ')} ${covered ? '│' : '━'} `
 		let min_file_line_coverage = report.min_file_line_coverage.expected
 
@@ -52,36 +65,44 @@ export function print({ report, context }: Report, params: CliArguments) {
 					sheet.line_coverage_ratio < min_file_line_coverage &&
 					params['show-uncovered'] === 'violations')
 			) {
-				console.log()
-				console.log(styleText('dim', '─'.repeat(terminal_width)))
-				console.log(sheet.url)
-				console.log(`Coverage: ${(sheet.line_coverage_ratio * 100).toFixed(2)}%, ${sheet.covered_lines}/${sheet.total_lines} lines covered`)
+				output.push()
+				output.push(styleText('dim', '─'.repeat(print_width)))
+				output.push(sheet.url)
+				output.push(`Coverage: ${percentage(sheet.line_coverage_ratio)}, ${sheet.covered_lines}/${sheet.total_lines} lines covered`)
 
 				if (min_file_line_coverage && min_file_line_coverage !== 0 && sheet.line_coverage_ratio < min_file_line_coverage) {
 					let lines_to_cover = min_file_line_coverage * sheet.total_lines - sheet.covered_lines
-					console.log(`Tip: cover ${Math.ceil(lines_to_cover)} more lines to meet the file threshold of ${min_file_line_coverage * 100}%`)
+					output.push(
+						`Tip: cover ${Math.ceil(lines_to_cover)} more lines to meet the file threshold of ${percentage(min_file_line_coverage)}`,
+					)
 				}
-				console.log(styleText('dim', '─'.repeat(terminal_width)))
+				output.push(styleText('dim', '─'.repeat(print_width)))
 
 				let lines = sheet.text.split('\n')
 
 				for (let chunk of sheet.chunks.filter((chunk) => !chunk.is_covered)) {
 					// Render N leading lines
 					for (let x = Math.max(chunk.start_line - NUM_LEADING_LINES, 0); x < chunk.start_line; x++) {
-						console.log(styleText('dim', line_number(x)), styleText('dim', indent(lines[x - 1])))
+						output.push([styleText('dim', line_number(x)), styleText('dim', indent(lines[x - 1]))].join(''))
 					}
 					// Render the uncovered chunk
 					for (let i = chunk.start_line; i <= chunk.end_line; i++) {
-						console.log(styleText('red', line_number(i, false)), indent(lines[i - 1]))
+						output.push([styleText('red', line_number(i, false)), indent(lines[i - 1])].join(''))
 					}
 					// Render N trailing lines
 					for (let y = chunk.end_line; y < Math.min(chunk.end_line + NUM_TRAILING_LINES, lines.length); y++) {
-						console.log(styleText('dim', line_number(y)), styleText('dim', indent(lines[y - 1])))
+						output.push([styleText('dim', line_number(y)), styleText('dim', indent(lines[y - 1]))].join(''))
 					}
 					// Show empty line between blocks
-					console.log()
+					output.push()
 				}
 			}
 		}
 	}
+
+	return output
+}
+
+export function print(report: Report, params: CliArguments): void {
+	print_lines(report, params, { styleText, print_width: process.stdout.columns }).map((line) => console.log(line))
 }
